5b5d78efa3ae43fa22f1e7a867f690b7d79964c5
Francesca Panteli <francescapanteli@Mac.mynet>
2025-08-15 21:30:28 +0100

    This commit pushes solutions to the 5th exercise

--- codingnomads/resources/07_variables-and-types/07_04_multiple_improve.py
+++ codingnomads/resources/07_variables-and-types/07_04_multiple_improve.py
@@ -1,4 +1,7 @@
 # Improve the variable assignment shown below.
 # Why should you change it?
 
-color, year = "red", 1900
+# color, year = "red", 1900
+# the two variables are unrelated, put them on different lines
+color = "red"
+year = 1900
--- codingnomads/resources/07_variables-and-types/07_06_whats_my_type.py
+++ codingnomads/resources/07_variables-and-types/07_06_whats_my_type.py
@@ -2,6 +2,10 @@
 # What data types does it hold at certain points earlier in the script?
 
 mystery = None
+print("datatype 1: ",type(mystery))
 mystery = "Sommerfeld"
+print("datatype 2: ",type(mystery))
 mystery = 137
+print("datatype 3: ",type(mystery))
 mystery = mystery + 0.0
+print("datatype 4: ",type(mystery))
